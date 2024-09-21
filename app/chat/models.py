c7c7e12511c13accfabd665e29e9bf1650bab0d7
vyordanov <vyordanov@godaddy.com>
2024-09-21 18:45:02 +0300

    Create chat rooms model

--- app/chat/models.py
+++ app/chat/models.py
@@ -13,3 +13,33 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username}'
+
+
+class ChatRoom(models.Model):
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        blank=False,
+        null=False,
+        unique=True,
+        editable=False
+    )
+    uuid_redacted = models.CharField(
+        default='',
+        blank=True,
+        null=True,
+        editable=False
+    )
+
+
+class UserChatRoom(models.Model):
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+    )
+    chat_room = models.ForeignKey(
+        to=ChatRoom,
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        unique_together = (('user', 'chat_room'),)
