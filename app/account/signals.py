b7a53d3bc743fc98cedc450fb6988d3ab6247c90
vyordanov <vyordanov@godaddy.com>
2024-11-05 21:16:38 +0200

    Add post_migrate signal on account

--- app/account/signals.py
+++ app/account/signals.py
@@ -1,7 +1,8 @@
-from django.db.models.signals import post_save
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models.signals import post_save, post_migrate
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Themes
 
 
 @receiver(post_save, sender=User)
@@ -12,3 +13,16 @@ def create_user_profile(sender, instance, created, **kwargs):
     """
     if created:
         Profile.objects.create(user=instance)
+
+
+@receiver(post_migrate)
+def create_default_themes(sender, **kwargs):
+    """
+    Create a signal that will automatically create the default themes ( dark/light )
+    """
+    required = ['dark', 'light']
+    for entry in required:
+        try:
+            Themes.objects.get(name=entry.lower())
+        except ObjectDoesNotExist as e:
+            Themes.objects.create(name=entry.lower())
