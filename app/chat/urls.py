e5312784f5581676e2f3ebf2a08ca1b9e45c394d
vyordanov <vyordanov@godaddy.com>
2024-11-06 16:18:42 +0200

    Change index page URLs

--- app/chat/urls.py
+++ app/chat/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('<str:room_name>/', ChatRoomView.as_view(), name='room'),
     path('group/<str:group_uuid>/', GroupChatView.as_view(), name='group_chat'),
     path('api/send-message/', send_message, name='send_message'),
-    path('', MembersView.as_view(), name='none'),
 ]
